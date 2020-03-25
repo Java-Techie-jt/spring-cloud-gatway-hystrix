aa0452f7c2d1f11f46cf53c104b2f21352bd9577
Basant kumar Hota <basahota@users.noreply.github.com>
2020-03-25 18:25:34 +0530

    Update OrderServiceApplication.java

--- spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/OrderServiceApplication.java
+++ spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/OrderServiceApplication.java
@@ -2,16 +2,20 @@ package com.javatechie.os.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 	 @Bean
+	 @LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
