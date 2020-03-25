4e8899102b4dc944f7180d77575880f8320a89c8
Basant kumar Hota <basahota@users.noreply.github.com>
2020-03-25 19:24:24 +0530

    Update PaymentServiceApplication.java

--- spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/PaymentServiceApplication.java
+++ spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.javatechie.ps.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
