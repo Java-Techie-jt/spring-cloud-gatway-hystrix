dee4e8558f45abb2f903c2af277c69555d77e09c
Basant kumar Hota <basahota@users.noreply.github.com>
2020-03-26 23:30:39 +0530

    Update PaymentController.java

--- spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/controller/PaymentController.java
+++ spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/controller/PaymentController.java
@@ -3,10 +3,7 @@ package com.javatechie.ps.api.controller;
 import com.javatechie.ps.api.entity.Payment;
 import com.javatechie.ps.api.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -20,6 +17,11 @@ public class PaymentController {
     @PostMapping("/doPayment")
     public Payment doPayment(@RequestBody Payment payment) {
         return service.doPayment(payment);
+    }
+
+    @GetMapping("/{orderId}")
+    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId){
+              return service.findPaymentHistoryByOrderId(orderId);
     }
 
 
