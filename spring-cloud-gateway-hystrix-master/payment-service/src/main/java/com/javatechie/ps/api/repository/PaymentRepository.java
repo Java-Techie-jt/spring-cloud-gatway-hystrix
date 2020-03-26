cf9f3e2cccfd0f841d9a91c34d04157dd5bfc78f
Basant kumar Hota <basahota@users.noreply.github.com>
2020-03-26 23:31:37 +0530

    Update PaymentRepository.java

--- spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/repository/PaymentRepository.java
+++ spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/repository/PaymentRepository.java
@@ -4,4 +4,6 @@ import com.javatechie.ps.api.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+    Payment findByOrderId(int orderId);
 }
+
