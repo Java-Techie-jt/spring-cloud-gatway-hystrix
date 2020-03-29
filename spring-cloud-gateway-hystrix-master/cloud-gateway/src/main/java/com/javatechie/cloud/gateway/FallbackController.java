3c6e498d28f321d95f2573c5d68d293b645783b7
Basant kumar Hota <basahota@users.noreply.github.com>
2020-03-30 01:00:13 +0530

    Create FallbackController.java

---
+++ spring-cloud-gateway-hystrix-master/cloud-gateway/src/main/java/com/javatechie/cloud/gateway/FallbackController.java
@@ -0,0 +1,18 @@
+package com.javatechie.cloud.gateway;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/orderFallBack")
+    public Mono<String> orderServiceFallBack() {
+        return Mono.just("Order Service is taking too long to respond or is down. Please try again later");
+    }
+    @RequestMapping("/paymentFallback")
+    public Mono<String> paymentServiceFallBack() {
+        return Mono.just("Payment Service is taking too long to respond or is down. Please try again later");
+    }
+}
