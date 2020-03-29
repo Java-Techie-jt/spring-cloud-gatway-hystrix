6c136aa4f48f4ae24a0a6ec354f7c173c6d9f250
Basant kumar Hota <basahota@users.noreply.github.com>
2020-03-30 00:59:08 +0530

    Update CloudGatewayApplication.java

--- spring-cloud-gateway-hystrix-master/cloud-gateway/src/main/java/com/javatechie/cloud/gateway/CloudGatewayApplication.java
+++ spring-cloud-gateway-hystrix-master/cloud-gateway/src/main/java/com/javatechie/cloud/gateway/CloudGatewayApplication.java
@@ -3,9 +3,11 @@ package com.javatechie.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
