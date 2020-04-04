476f1ca7278a7f02be8b1a2ba47efed79e477e75
Basant kumar Hota <basahota@users.noreply.github.com>
2020-04-04 14:00:59 +0530

    #cloud config server

---
+++ cloud-config-server/src/test/java/com/javatechie/cloud/config/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
