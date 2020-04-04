8c144f5bf6ff3fca36e532b7be6058916beabac2
Basant kumar Hota <basahota@users.noreply.github.com>
2020-04-04 14:12:27 +0530

    Update OrderService.java

--- spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
+++ spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
@@ -6,16 +6,24 @@ import com.javatechie.os.api.common.TransactionResponse;
 import com.javatechie.os.api.entity.Order;
 import com.javatechie.os.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository repository;
     @Autowired
+    @Lazy
     private RestTemplate template;
+
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
         String response = "";
@@ -24,7 +32,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         //rest call
-        Payment paymentResponse = template.postForObject("http://localhost:9191/payment/doPayment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
 
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successful and order placed" : "there is a failure in payment api , order added to cart";
 
