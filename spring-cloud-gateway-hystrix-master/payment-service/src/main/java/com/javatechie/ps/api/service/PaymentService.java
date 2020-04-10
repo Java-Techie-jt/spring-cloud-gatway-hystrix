18f12b792c24b62bce127a19588feb3f2b5dec25
Basant kumar Hota <basahota@users.noreply.github.com>
2020-04-10 21:39:06 +0530

    logger

--- spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/service/PaymentService.java
+++ spring-cloud-gateway-hystrix-master/payment-service/src/main/java/com/javatechie/ps/api/service/PaymentService.java
@@ -1,7 +1,11 @@
 package com.javatechie.ps.api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javatechie.ps.api.entity.Payment;
 import com.javatechie.ps.api.repository.PaymentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +14,17 @@ import java.util.UUID;
 
 @Service
 public class PaymentService {
+    
     @Autowired
     private PaymentRepository repository;
-
-    public Payment doPayment(Payment payment){
+    
+    Logger logger= LoggerFactory.getLogger(PaymentService.class);
+    
+    public Payment doPayment(Payment payment) throws JsonProcessingException {
         payment.setPaymentStatus(paymentProcessing());
         payment.setTransactionId(UUID.randomUUID().toString());
+        logger.info("Payment-Service Request : {}",new ObjectMapper().writeValueAsString(payment));
+
         return repository.save(payment);
     }
 
@@ -27,6 +36,8 @@ public class PaymentService {
 
 
     public Payment findPaymentHistoryByOrderId(int orderId) {
-       return repository.findByOrderId(orderId);
+        Payment payment=repository.findByOrderId(orderId);
+        logger.info("paymentService findPaymentHistoryByOrderId : {}",new ObjectMapper().writeValueAsString(payment));
+        return payment ;
     }
 }
