b745f93bf9f561999b70edd7ffbe403259d3c0a0
Basant kumar Hota <basahota@users.noreply.github.com>
2020-04-10 21:33:25 +0530

    log statement added

--- spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
+++ spring-cloud-gateway-hystrix-master/order-service/src/main/java/com/javatechie/os/api/service/OrderService.java
@@ -1,10 +1,14 @@
 package com.javatechie.os.api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javatechie.os.api.common.Payment;
 import com.javatechie.os.api.common.TransactionRequest;
 import com.javatechie.os.api.common.TransactionResponse;
 import com.javatechie.os.api.entity.Order;
 import com.javatechie.os.api.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -16,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @RefreshScope
 public class OrderService {
 
+    Logger logger= LoggerFactory.getLogger(OrderService.class);
     @Autowired
     private OrderRepository repository;
     @Autowired
@@ -25,17 +30,18 @@ public class OrderService {
     @Value("${microservice.payment-service.endpoints.endpoint.uri}")
     private String ENDPOINT_URL;
 
-    public TransactionResponse saveOrder(TransactionRequest request) {
+    public TransactionResponse saveOrder(TransactionRequest request) throws JsonProcessingException {
         String response = "";
         Order order = request.getOrder();
         Payment payment = request.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         //rest call
+        logger.info("Order-Service Request : "+new ObjectMapper().writeValueAsString(request));
         Payment paymentResponse = template.postForObject(ENDPOINT_URL, payment, Payment.class);
 
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successful and order placed" : "there is a failure in payment api , order added to cart";
-
+        logger.info("Order Service getting Response from Payment-Service : "+new ObjectMapper().writeValueAsString(response));
         repository.save(order);
         return new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
     }
